ae9b81a772e450e0d9ee5ab4a4939d5a6a87ba43
サム <118713201+bowdensl712@users.noreply.github.com>
2022-12-04 18:30:29 +0900

    イメージを追加 ・/images/backgroundsフォルダーを追加 ・streetCorner.jpgを追加 ・イメージを表示 ・cssでイメージのサイズ、marginを変更

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const text1 = document.createElement("p");
 const text2 = document.createElement("p");
 const text3 = document.createElement("p");
 const text4 = document.createElement("p");
+const image1 = document.createElement("img");
+image1.setAttribute("class", "image");
 
 
 //Functions
@@ -45,9 +47,10 @@ loadStreetCorner();
 function loadStreetCorner() {
     clear(container);
     title1.innerText = "Street Corner";
-    container.append(title1);
-
-
+    text1.innerText = "You are standing on a street corner. \n You've just moved to the city, and you're not quite sure where to go from here. "
+    image1.src = "images/backgrounds/streetCorner.jpg";
+    container.append(title1, text1, image1);
+   
 };
 
 
