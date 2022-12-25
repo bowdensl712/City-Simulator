30c38f34bcd76c79f6f0c7fc905633e791f1448f
サム <118713201+bowdensl712@users.noreply.github.com>
2022-12-25 21:29:21 +0900

    diaryBedroom()関数を追加

--- diaryscript.js
+++ diaryscript.js
@@ -1,20 +1,46 @@
-//Modern-day Variables
+//現代の変数
 let modernDate;
 let modernMoney;
 let modernInventory;
 
-//Diary Variables
+//日記の変数
 let diaryOpened = false;
+let diaryMoney = 2000; //80年代の円
+let diaryDate = new Date("1986-04-01T07:00:00");
+let diaryInventory = [];
+
+//一般の関数
+function hiduke() {
+    return diaryDate.toLocaleString("ja-JP", {year: "numeric", month: "long", day: "numeric"}) + "：";
+};
 
 function loadDiary() { 
     modernDate = JSON.parse(JSON.stringify(date));
     modernMoney = JSON.parse(JSON.stringify(money));
     modernInventory = JSON.parse(JSON.stringify(inventoryContents));
     if (diaryOpened === false) {
-        
-
+        clear(container);
+        title1.innerText = hiduke() + "私の部屋";
+        text1.innerText = "今日から私の新しい生活が始まる！\n新しい学校、新しい制服、新しいクラスメイト...\n未来には何が待っているのかな？";
+        image1.src = "images/1986/backgrounds/diaryBedroom.jpg";
+        link1.innerText = "廊下に出る";
+        link1.setAttribute("onclick", "diaryHallway()");
+        container.append(title1, text1, image1, link1);
         diaryOpened = true;
     } else { 
-
+        diaryBedroom();
     };
 };
+
+
+function diaryBedroom() {
+    clear(container);
+    title1.innerText = hiduke() + "私の部屋";
+    text1.innerText = "今日から私の新しい生活が始まる！\n新しい学校、新しい制服、新しいクラスメイト...\n未来には何が待っているのかな？";
+    image1.src = "images/1986/backgrounds/diaryBedroom.jpg";
+    link1.innerText = "廊下に出る";
+    link1.setAttribute("onclick", "diaryHallway()");
+    container.append(title1, text1, image1, link1);
+
+};
+
